4f1625d83d49812ef697b3fa664d26ff9fe667cc
W00lenW0lf <igor45641@gmail.com>
2025-09-30 18:34:26 +0700

    homework2

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ def download_hh_vacanies(language):
     found = 0
     while True:
         params = {
-            "text": language, #ключевые слова поиска, используем названия языков программирования
-            "per_page": 100, #количество вакансий на странице
-            "page": page, #номер страницы для их перебора
-            "area": 113, #где ищем вакансии,113 - Москва
-            "only_with_salary": "true", #берем только вакансии с указанной зарплатой
-            "period": "30" #за какой период собираем вакансии, 30 дней по умолчанию
+            "text": language,  # ключевые слова поиска, используем названия языков программирования
+            "per_page": 100,  # количество вакансий на странице
+            "page": page,  # номер страницы для их перебора
+            "area": 113,  # где ищем вакансии,113 - Москва
+            "only_with_salary": "true",  # берем только вакансии с указанной зарплатой
+            "period": "30"  # за какой период собираем вакансии, 30 дней по умолчанию
         }
         response = requests.get(url, params=params)
         response.raise_for_status()
@@ -43,11 +43,11 @@ def download_superjob_vacanies(language):
     found = 0
     while True:
         params = {
-            "catalogues": "Разработка, программирование", #каталог, в котором ищем вакансии
-            "keyword": language, #ключевые слова поиска, используем названия языков программирования
+            "catalogues": "Разработка, программирование",  # каталог, в котором ищем вакансии
+            "keyword": language,  # ключевые слова поиска, используем названия языков программирования
             "town": 4,  # Москва
             "count": 50,  # Количество вакансий на страницу
-            "page": page #номер страницы для их перебора
+            "page": page  # номер страницы для их перебора
         }
         response = requests.get(url, headers=headers, params=params)
         response.raise_for_status()
@@ -62,7 +62,7 @@ def download_superjob_vacanies(language):
     return found, all_vacancies
 
 
-def average_salary_calculator(min_salary, max_salary):
+def calculate_average_salary(min_salary, max_salary):
     if min_salary and max_salary:
         return (max_salary + min_salary) / 2
     elif min_salary:
@@ -72,42 +72,44 @@ def average_salary_calculator(min_salary, max_salary):
     else:
         return None
 
+
 def predict_rub_salary_for_hh(vacancy):
     salary = vacancy["salary"]
-    if salary['currency'] == 'RUR':
-        min_salary = salary['from']
-        max_salary = salary['to']
-        return average_salary_calculator(min_salary, max_salary)
-    else:
+    if not salary['currency'] == 'RUR':
         return None
+    min_salary = salary['from']
+    max_salary = salary['to']
+    return calculate_average_salary(min_salary, max_salary)
 
 
 def predict_rub_salary_for_superJob(vacancy):
     min_salary = vacancy.get('payment_from', 0)
     max_salary = vacancy.get('payment_to', 0)
-    return average_salary_calculator(min_salary, max_salary)
+    return calculate_average_salary(min_salary, max_salary)
+
 
 def make_table(title, rows):
     header = ['Язык программирования', 'Вакансий найдено',
-                            'Вакансий обработано', 'Средняя зарплата']
+              'Вакансий обработано', 'Средняя зарплата']
 
     table_data = [[title], header] + rows
     return AsciiTable(table_data).table
 
-def collect_salary_statistics(sources, languages):
-    for cite, download_function, predict_function in sources:
-        rows = []
-        for language in languages:
-            found, items = download_function(language)
-            salaries = []
-            for vacancy in items:
-                predicted_salary = predict_function(vacancy)
-                if predicted_salary:
-                    salaries.append(predicted_salary)
-            vacancies_processed = len(salaries)
-            average_salary = int(sum(salaries) / vacancies_processed) if salaries else 0
-            rows.append([language, found, vacancies_processed, average_salary])
-        print(make_table(cite, rows))
+
+def collect_salary_statistics(source, languages):
+    cite, download_function, predict_function = source
+    rows = []
+    for language in languages:
+        vacancies_quantity, all_vacancies = download_function(language)
+        salaries = []
+        for vacancy in all_vacancies:
+            predicted_salary = predict_function(vacancy)
+            if predicted_salary:
+                salaries.append(predicted_salary)
+        vacancies_processed = len(salaries)
+        average_salary = int(sum(salaries) / vacancies_processed) if salaries else 0
+        rows.append([language, vacancies_quantity, vacancies_processed, average_salary])
+    return (rows)
 
 
 if __name__ == '__main__':
@@ -115,6 +117,9 @@ if __name__ == '__main__':
     languages = ("java", "python", "javascript")
     sources = [
         ("SuperJob Moscow", download_superjob_vacanies, predict_rub_salary_for_superJob),
-        ("Headhunter Moscow", download_hh_vacanies, predict_rub_salary_for_hh),
+        ("Headhunter Moscow", download_hh_vacanies, predict_rub_salary_for_hh)
     ]
-    collect_salary_statistics(sources, languages)
+    for source in sources:
+        rows = collect_salary_statistics(source, languages)
+        cite = source[0]
+        print(make_table(cite, rows))
